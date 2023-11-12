f61af7649f7c63a2863f4c210cb80d329d009fdb
hmnhGeek <hmnhsharma97@gmail.com>
2023-11-12 21:17:48 +0530

    removed unused imports

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link';
 import styles from './login.module.css';
 import cookie from 'js-cookie';
 import toast, { Toaster } from 'react-hot-toast';
-import {useDispatch, useSelector} from "react-redux";
 import { login, logout } from '@/redux/actions/authActions';
 import { RootState, AppDispatch } from '@/redux/store';
 import { connect } from 'react-redux';
--- src/app/platforms/page.tsx
+++ src/app/platforms/page.tsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { fetchAvailablePlatforms } from "@/redux/actions/platformsActions";
 import { RootState, AppDispatch } from "@/redux/store";
-import { useDispatch, useSelector } from "react-redux";
 import { connect } from "react-redux";
 
 interface PlatformsPageProps {
