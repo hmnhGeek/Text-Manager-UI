fcd52fa8ee9b121796cb05b5436a1ade6cfe6143
hmnhGeek <hmnhsharma97@gmail.com>
2023-11-18 13:27:05 +0530

    added formik and yup for form validations

--- src/app/components/AddPlatformForm/AddPlatformForm.tsx
+++ src/app/components/AddPlatformForm/AddPlatformForm.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
 import styles from './AddPlatfromForm.module.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object(
+  {
+    platformName: Yup.string().required("Platform name is required!"),
+    title: Yup.string().required("Title is required!")
+  }
+);
 
 // interface AddPlatformFormProps {
 //   onSubmit: (platformName: string, title: string) => void;
@@ -7,8 +16,19 @@ import styles from './AddPlatfromForm.module.css';
 // }
 
 const AddPlatformForm = () => {
-  const [platformName, setPlatformName] = useState('');
-  const [title, setTitle] = useState('');
+  const formik = useFormik(
+    {
+      initialValues: {
+        platformName: "",
+        title: ""
+      },
+      validationSchema: validationSchema,
+      onSubmit: values => {
+        console.log(values);
+        formik.resetForm();
+      }
+    }
+  );
 
 //   const handleSubmit = () => {
 //     onSubmit(platformName, title);
@@ -18,27 +38,41 @@ const AddPlatformForm = () => {
 
   return (
     <div>
-      <label className={styles.addlabel}>
-        Platform Name:
-        <input
-          type="text"
-          value={platformName}
-          onChange={(e) => setPlatformName(e.target.value)}
-          className={styles.addinput}
-        />
-      </label>
-      <label className={styles.addlabel}>
-        Title:
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className={styles.addinput}
-        />
-      </label>
-      <button className={styles.submitButton}>
-        Submit
-      </button>
+      <form onSubmit={formik.handleSubmit}>
+        <label className={styles.addlabel}>
+          Platform Name:
+          <input
+            type="text"
+            value={formik.values.platformName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`${styles.addinput} ${formik.touched.platformName && formik.errors.platformName ? styles.errorHighlight : ''}`}
+          />
+          {
+            formik.touched.platformName && formik.errors.platformName && (
+              <div className={styles.error}>{formik.errors.platformName}</div>
+            )
+          }
+        </label>
+        <label className={styles.addlabel}>
+          Title:
+          <input
+            type="text"
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`${styles.addinput} ${formik.touched.platformName && formik.errors.platformName ? styles.errorHighlight : ''}`}
+          />
+          {
+            formik.touched.title && formik.errors.title && (
+              <div className={styles.error}>{formik.errors.title}</div>
+            )
+          }
+        </label>
+        <button type='submit' className={styles.submitButton}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
